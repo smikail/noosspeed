3e1f4df25edd8072a06b251a7bdbd8f6bbbfd73a
mehrparvar <m.r.mehrparvar1@gmail.com>
2016-01-24 18:29:58 +0330

    Update st.lua

--- plugins/st.lua
+++ plugins/st.lua
@@ -5,3 +5,15 @@ local function run(msg)
 if msg.text == "عشقم" then
 	return "بله عزیزم ؟"
 end
+end
+
+return {
+	description = "Chat With Robot Server", 
+	usage = "chat with robot",
+	patterns = {
+	       	"^(عشقم)$",
+	       			}, 
+	run = run,
+    --privileged = true,
+	pre_process = pre_process
+}
