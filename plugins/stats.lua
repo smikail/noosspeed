b804594027e1f661fef8109e7511f1ec6ef31525
mehrparvar <m.r.mehrparvar1@gmail.com>
2016-01-24 03:44:51 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'creedbot' then -- Put everything you like :)
+  if matches[1]:lower() == 'noosspeed' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /creedbot ")
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "creedbot" then -- Put everything you like :)
+    if matches[2] == "noosspeed" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -141,8 +141,13 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (creedbot)",-- Put everything you like :)
-		"^[!/]([Cc]reedbot)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (noosspeed)",-- Put everything you like :)
+		"^[!/]([Nn]oosspeed)",-- Put everything you like :)
+    "^([Ss]tats)$",
+    "^([Ss]tatslist)$",
+    "^([Ss]tats) (group) (%d+)",
+    "^([Ss]tats) (noosspeed)",-- Put everything you like :)
+		"^([Nn]oosspeed)"-- Put everything you like :)	
     }, 
   run = run
 }
