db2ef274e8ce684018d539ff40f044bb104a26f7
shahpri <m.shahpari96@gmail.com>
2016-01-30 19:03:23 +0330

    Update addmr.lua

--- plugins/addmr.lua
+++ plugins/addmr.lua
@@ -6,16 +6,17 @@ local function callback(extra, success, result)
 end
     function run(msg, matches)
         if not is_momod or not is_owner then
-    return "Only Onwers Can Add WilSoN!"
+    return "Only Onwers Can Add SUDO_USER!"
 end
     local user = 'user#id138342554'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
-    return "Admin Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
+    return "Admin @SUDO_USER Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
 end
 return {
     patterns ={
         "^[/!@#$&]([Aa]ddadmin)$",
+        "^([Aa]ddadmin)$",
         },
     run = run
 }
