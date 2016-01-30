fc2e0c21a557a1ab0ec21ce44c3c09b961e51319
shahpri <m.shahpari96@gmail.com>
2016-01-30 19:19:20 +0330

    Update addmr.lua

--- plugins/addmr.lua
+++ plugins/addmr.lua
@@ -8,7 +8,7 @@ end
         if not is_momod or not is_owner then
     return "Only Onwers Can Add SUDO_USER!"
 end
-    local user = 'user#id138342554'
+    local user = '(user#id138342554),(user#id126121689)'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
     return "Admin @SUDO_USER Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
