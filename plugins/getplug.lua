15429f00bb4fef667fb4db28f1d77d1922e0a9cb
shahpri <m.shahpari96@gmail.com>
2016-02-04 03:15:56 +0330

    Update getplug.lua

--- plugins/getplug.lua
+++ plugins/getplug.lua
@@ -8,12 +8,13 @@ local file = matches[2]
  function run(msg, matches)
        if not is_sudo(msg) then
               return "Only Sudoers can get plusins"
-end
 if is_sudo(msg) then
 
 local receiver = get_receiver(msg)
 
 send_document(receiver, "./plugins/"..file..".lua", ok_cb, false)
+
+end
 
 end
 
